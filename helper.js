c1784302aa28730f0d1625023764cc1c421b27be
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-27 11:41:15 -0400

    Toronto Path 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,11 +1,5 @@
 $('document').ready(function() {
-    $('#menu-icon').click(function(){
-		$(this).toggleClass('open');
-		$('#menu_page').slideToggle(100,"linear");
-		$('#menu-icon').toggle();
-		$('<div class="modal-backdrop custom_backdrop"></div>').appendTo(document.body);
-        // $('.logo_container').toggle();
-	});
+    
 });
 function init(e){
        $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
