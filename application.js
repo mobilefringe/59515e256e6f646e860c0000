fdcdb4d8315aad45fff34846cb3ad0d4b2d968b2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-27 11:30:26 -0400

    Toronto Path 1.0  /application.js

--- application.js
+++ application.js
@@ -1,9 +1,4 @@
 /*Created 2015-02-28 by CodeCloud Team*/
-
-function test_function(){
-    alert('calling test_function');
-}
-
 function init(){
     $('#menu-icon').click(function(){
 		$(this).toggleClass('open');
