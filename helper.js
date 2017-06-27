62725af56599d91ec022bd04780e4b104fb98392
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-27 11:37:08 -0400

    Toronto Path 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -8,7 +8,7 @@ $('document').ready(function() {
 	});
 });
 function init(e){
-   
+       $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
 	
     // $('#open_menu').click(function(){
     //     $('#mobile_menu').slideDown();
