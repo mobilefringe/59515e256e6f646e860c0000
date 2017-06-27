9c19226ca339c3175e317ab5dfb93230b0a3a658
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-27 14:14:22 -0400

    Toronto Path 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -22,42 +22,42 @@ $('document').ready(function(e) {
         showSearchResults();
     });
     function showSearchResults(){
-    $('#search_results').show();
-    if($('#search_input').val().length === 0){
-        $('#search_results').hide();
-    }else{
-        var search_results = getSearchResults($('#search_input').val(),99,100);
-        $('.search-results-count').html("Total Results : "+search_results.summary.count);
-        renderSearchResultsTemplate('#search_results_template','#search_results_items',search_results);
-        if (search_results["stores"]){
-            if (search_results["stores"].length > 0){
-                $("#store_results_header").html(search_results["stores"].length+" Stores <i id='store_arrow' class='fa fa-chevron-right pull-right'></i>") ;
-                $("#store_results_header").show();
+        $('#search_results').show();
+        if($('#search_input').val().length === 0){
+            $('#search_results').hide();
+        }else{
+            var search_results = getSearchResults($('#search_input').val(),99,100);
+            $('.search-results-count').html("Total Results : "+search_results.summary.count);
+            renderSearchResultsTemplate('#search_results_template','#search_results_items',search_results);
+            if (search_results["stores"]){
+                if (search_results["stores"].length > 0){
+                    $("#store_results_header").html(search_results["stores"].length+" Stores <i id='store_arrow' class='fa fa-chevron-right pull-right'></i>") ;
+                    $("#store_results_header").show();
+                }
+                
+            } else {
+                $("#store_results_header").hide();
             }
-            
-        } else {
-            $("#store_results_header").hide();
-        }
-        if (search_results["promotions"]){
-            if (search_results["promotions"].length > 0){
-                $("#promotions_results_header").html(search_results["promotions"].length+" Promotions <i id='promo_arrow' class='fa fa-chevron-right pull-right'></i>")    ;
-                $("#promotions_results_header").show();
+            if (search_results["promotions"]){
+                if (search_results["promotions"].length > 0){
+                    $("#promotions_results_header").html(search_results["promotions"].length+" Promotions <i id='promo_arrow' class='fa fa-chevron-right pull-right'></i>")    ;
+                    $("#promotions_results_header").show();
+                }
+                
+            } else {
+                $("#promotions_results_header").hide();
             }
-            
-        } else {
-            $("#promotions_results_header").hide();
-        }
-        if (search_results["events"]){
-            if (search_results["events"].length > 0) {
-                $("#events_results_header").html(search_results["events"].length+" Events <i id='event_arrow' class='fa fa-chevron-right pull-right'></i>")
-                $("#events_results_header").show();
+            if (search_results["events"]){
+                if (search_results["events"].length > 0) {
+                    $("#events_results_header").html(search_results["events"].length+" Events <i id='event_arrow' class='fa fa-chevron-right pull-right'></i>")
+                    $("#events_results_header").show();
+                }
+                
+            } else {
+                $("#events_results_header").hide();
             }
-            
-        } else {
-            $("#events_results_header").hide();
         }
     }
-}
 });
 function init(e){
     
