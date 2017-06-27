c7b3008d8847850b8615c9d172914e5fb7702181
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-27 11:35:50 -0400

    Toronto Path 1.0  /application.js

--- application.js
+++ application.js
@@ -1,29 +1,4 @@
 /*Created 2015-02-28 by CodeCloud Team*/
-function init(){
-    $('#menu-icon').click(function(){
-		$(this).toggleClass('open');
-		$('#menu_page').slideToggle(100,"linear");
-		$('#menu-icon').toggle();
-		$('<div class="modal-backdrop custom_backdrop"></div>').appendTo(document.body);
-        // $('.logo_container').toggle();
-	});
-	
-    // $('#open_menu').click(function(){
-    //     $('#mobile_menu').slideDown();
-    //     $(this).hide();
-    //     $('#close_menu').show();
-    // });
-    
-    $('.close_menu').click(function(){
-        $('#menu_page').slideUp();
-        //$(this).hide();
-        $('#menu-icon').toggle();
-        $(".custom_backdrop").remove();
-        // $('.logo_container').toggle();
-    });
-	
-}
-
 function renderGeneral(container, template, collection){
     var item_list = [];
     var item_rendered = [];
