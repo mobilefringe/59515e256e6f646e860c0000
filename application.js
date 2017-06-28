4bb63f8ba0579483e1c87ad56500f3f681c0d255
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-28 11:14:59 -0400

    Toronto Path 1.0  /application.js

--- application.js
+++ application.js
@@ -386,7 +386,7 @@ function renderPromotions(container, template, collection){
             
         }
         else{
-            val.store_name = "St. Vital Centre";
+            val.store_name = "The Toronto Path";
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/592482696e6f6450ebc40000/image/png/1495569752000/logo.png";
         }
         
@@ -441,7 +441,7 @@ function renderPromoDetails(container, template, collection){
             }
         }
         else{
-            val.store_name = "St. Vital Centre";
+            val.store_name = "The Toronto Path";
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/592482696e6f6450ebc40000/image/png/1495569752000/logo.png";
         }
         
@@ -580,7 +580,7 @@ function renderEventDetails(container, template, collection){
             }
         }
         else{
-            val.store_name = "St. Vital Centre";
+            val.store_name = "The Toronto Path";
             val.image_url  = val.event_image_url_abs;
             if(val.image_url.indexOf('missing.png') > 0){
                 val.image_url = "//codecloud.cdn.speedyrails.net/sites/592482696e6f6450ebc40000/image/png/1495569752000/logo.png";
