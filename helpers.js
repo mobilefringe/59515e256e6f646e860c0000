eb682236800f017c6a7825a5cb8659f1312afe7d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-28 14:06:39 -0400

    Toronto Path 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -87,7 +87,7 @@ function getDineStores () {
         // }
        // console.log("id=" ,val);
         console.log(getStoresListByCategoryID(parseInt(val)));
-        dine_stores.push(getStoresListByCategoryID(parseInt(val)));
+        dine_stores.concat(getStoresListByCategoryID(parseInt(val)));
        
     });
     
