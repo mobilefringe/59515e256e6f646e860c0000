1608079015f2b9a1627f8289338eb4e4b59a429a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-28 11:16:37 -0400

    Toronto Path 1.0  /application.js

--- application.js
+++ application.js
@@ -433,7 +433,7 @@ function renderPromoDetails(container, template, collection){
             val.image_url  = val.promo_image_url_abs;
             if(val.image_url.indexOf('missing.png') > 0){
                 if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
-                val.image_url = "//codecloud.cdn.speedyrails.net/sites/592482696e6f6450ebc40000/image/png/1495569752000/logo.png";
+                val.image_url = "//codecloud.cdn.speedyrails.net/sites/59515e256e6f646e860c0000/image/png/1498506681000/PathLogo2x.png";
                 }
                 else{
                     val.image_url = store_details.store_front_url_abs;
